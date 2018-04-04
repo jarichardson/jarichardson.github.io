dca7216359fb5eb48e88499411eed8c5b78c96d8
jarichardson <jacob.a.richardson@nasa.gov>
2018-04-04 12:45:08 -0400

    refreshed 3D images and model descriptions

--- js/ModelLightBox.js
+++ js/ModelLightBox.js
@@ -34,7 +34,7 @@ function hideModel() {
 					var stlname = 'APOLLO17-region.stl';
 					var imgsrc = 'images/3Dmodels/pic_taurus_littrow.jpg';
 					var modelTitle = 'Apollo 17 region';
-					var descrip = 'This is a zoom-out of the Apollo 17 Landing Site, the adjacent topography.';
+					var descrip = 'This is a zoom-out of the Apollo 17 Landing Site, showing the adjacent topography on the eastern rim of Mare Serenitatis. The landing site is in the valley near the center of the model, and the hills are members of the Taurus Mountains. Surrounding the Taurus-Littrow Valley landing site are the North, South, and East Massifs, and the Sculptured Hills, which might have been formed from the Imbrium impact.';
 					var geog = 'Roughly 6 km x 10 km';
 					var ve = '3x';
 					var datasrc = '<a href="http://wms.lroc.asu.edu/lroc/view_rdr/NAC_DTM_APOLLO17">LROC NAC DTM</a>';
@@ -47,14 +47,14 @@ function hideModel() {
 					var descrip = 'Ina D is the largest of the Irregular Mare Patches on the Moon and it is famously difficult to see in orthographic photos because the high-standing blobs of material look like they are depressions.';
 					var geog = 'Roughly 3 km wide';
 					var ve = '5x';
-					var datasrc = '';
+					var datasrc = '<a href="http://wms.lroc.asu.edu/lroc/view_rdr/NAC_DTM_INACALDERA1">LROC NAC DTM</a>';
 				break;
 				
 				case 'a11':
 					var stlname = 'APOLLO11.stl';
-					var imgsrc = 'images/3Dmodels/apollo/a11_modelview.png';
-					var modelTitle = 'Apollo 11';
-					var descrip = 'The Eagle landed near the center of this model and can be seen and felt as a bump. The nearest significant crater to this bump is Little West Crater, about 50 m to the east. This is the farthest the crew walked in the Apollo 11 mission. Some larger unnamed craters surround the mission area.';
+					var imgsrc = 'images/3Dmodels/pic_a11.gif';
+					var modelTitle = 'Apollo 11, Mare Tranquillitatis';
+					var descrip = 'The Eagle landed near the center of this model and its remaining descent stage can be seen and felt as a bump. The nearest recognizable crater to this bump is Little West Crater, about 50 m to the east. This is the farthest the crew walked in the Apollo 11 mission. Some larger unnamed craters surround the mission area.';
 					var geog = '1.1 km x 0.9 km centered on the mission area';
 					var ve = '3x';
 					var datasrc = '<a href="http://wms.lroc.asu.edu/lroc/view_rdr/NAC_DTM_APOLLO11">LROC NAC DTM</a>';
@@ -62,9 +62,9 @@ function hideModel() {
 				
 				case 'a12':
 					var stlname = 'APOLLO12.stl'
-					var imgsrc = 'images/3Dmodels/apollo/a12_modelview.png';
-					var modelTitle = 'Apollo 12';
-					var descrip = 'The broad crater in the top left is Middle Crescent Crater, and the LM came down near the second largest crater in the model, Surveyor crater (center-right).';
+					var imgsrc = 'images/3Dmodels/pic_a12.gif';
+					var modelTitle = 'Apollo 12, Surveyor Crater';
+					var descrip = 'Apollo 12 landed just 200 meters from the Surveyor 3 lander. The broad crater in the top left is Middle Crescent Crater, and the LM came down near the second largest crater in the model, Surveyor crater (center-right). The LM touched down on the northwest rim of Surveyor, while Surveyor 3 sits on the southwest rim.';
 					var geog = '1 km x 1 km centered on the landing site';
 					var ve = '3x';
 					var datasrc = '<a href="http://wms.lroc.asu.edu/lroc/view_rdr/NAC_DTM_APOLLO12">LROC NAC DTM</a>';
@@ -72,9 +72,9 @@ function hideModel() {
 				
 				case 'a14':
 					var stlname = 'APOLLO14.stl'
-					var imgsrc = 'images/3Dmodels/apollo/a14_modelview.png';
-					var modelTitle = 'Apollo 14';
-					var descrip = 'The crater at the top-right is Cone Crater. The LM landed near the two overlapping craters near the center of the model, North and Central craters.';
+					var imgsrc = 'images/3Dmodels/pic_a14.gif';
+					var modelTitle = 'Apollo 14, Fra Mauro Highlands';
+					var descrip = 'Apollo 14 landed in the Fra Mauro Highlands. The crater at the top-right is Cone Crater. The LM landed near the two overlapping craters near the center of the model, North and Central craters.';
 					var geog = '2.25 km x 1.6 km centered on the landing site';
 					var ve = '3x';
 					var datasrc = '<a href="http://wms.lroc.asu.edu/lroc/view_rdr/NAC_DTM_APOLLO14">LROC NAC DTM</a>';
@@ -82,8 +82,8 @@ function hideModel() {
 				
 				case 'a15':
 					var stlname = 'APOLLO15.stl'
-					var imgsrc = 'images/3Dmodels/apollo/a15_modelview.png';
-					var modelTitle = 'Apollo 15';
+					var imgsrc = 'images/3Dmodels/pic_a15.gif';
+					var modelTitle = 'Apollo 15, Hadley-Apennine Region';
 					var descrip = 'The main feature at the boundary of the Apollo 15 study area is Hadley Rille, making a large valley. To the south the model ends at the slope of the Apennine Front. The LM landed in the north central area of this model.';
 					var geog = '4.2 km x 6 km centered on the landing site';
 					var ve = '3x';
@@ -92,8 +92,8 @@ function hideModel() {
 				
 				case 'a16':
 					var stlname = 'APOLLO16.stl'
-					var imgsrc = 'images/3Dmodels/apollo/a16_modelview.png';
-					var modelTitle = 'Apollo 16';
+					var imgsrc = 'images/3Dmodels/pic_a16.gif';
+					var modelTitle = 'Apollo 16, Decartes Mountains';
 					var descrip = 'The Apollo 16 mission drove between Smoky Mountain and North Ray Crater at the top of the model to Stone Mountain in the bottom-right corner. The crew passed through Palmetto and Gator craters in the center of the model and initially landed near Spook crater, a small crater in the center.';
 					var geog = '11.5 km x 11.5 km centered on the landing site';
 					var ve = '3x';
@@ -101,9 +101,9 @@ function hideModel() {
 				break;
 				
 				case 'a17':
-					var stlname = 'APOLLO17.stl'
-					var imgsrc = 'images/3Dmodels/pic_taurus_littrow.jpg';
-					var modelTitle = 'Apollo 17';
+					var stlname = 'apollo17.stl'
+					var imgsrc = 'images/3Dmodels/pic_a17.gif';
+					var modelTitle = 'Apollo 17, Taurus Littrow Valley';
 					var descrip = 'The LM landed near the cluster of small craters in the center of Taurus-Littrow Valley. The valley extendes from the South Massif in the close corner to the North Massif and the large Sculptured Hills in the far corner. The Lee-Lincoln Scarp is seen in this model connecting the North and South Massifs.';
 					var geog = '5.8 km x 5 km centered on the landing site';
 					var ve = '3x';
